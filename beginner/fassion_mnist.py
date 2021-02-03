ce77b85846a7ee9746ca164c9c54c86d94f13a6b
yunseokddi <oys5339@naver.com>
2021-02-03 13:44:21 +0900

    overfitting and underfitting solve

--- beginner/fassion_mnist.py
+++ beginner/fassion_mnist.py
@@ -26,4 +26,16 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit()
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+print('\n테스트 정확도:', test_acc)
+
+predictions = model.predict(test_images)
+
+print(predictions[0])
+print(np.argmax(predictions[0]))
+print(test_labels[0])
+
+
---
+++ beginner/overfitting_underfitting.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+NUM_WORDS = 1000
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+
+def multi_hot_sequences(sequences, dimension):
+    results = np.zeros((len(sequences), dimension))
+
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0
+
+    return results
+
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
+
+baseline_model = keras.Sequential([
+    keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(16, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+baseline_model.compile(optimizer='adam',
+                       loss='binary_crossentropy',
+                       metrics=['accuracy', 'binary_crossentropy'])
+
+baseline_history = baseline_model.fit(train_data,
+                                      train_labels,
+                                      epochs=20,
+                                      batch_size=512,
+                                      validation_data=(test_data, test_labels),
+                                      verbose=2)
+
+smaller_model = keras.Sequential([
+    keras.layers.Dense(4, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(4, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+smaller_model.compile(optimizer='adam',
+                       loss='binary_crossentropy',
+                       metrics=['accuracy', 'binary_crossentropy'])
+
+smaller_history = smaller_model.fit(train_data,
+                                    train_labels,
+                                    epochs=20,
+                                    batch_size=512,
+                                    validation_data=(test_data, test_labels),
+                                    verbose=2)
+
+bigger_model = keras.Sequential([
+    keras.layers.Dense(512, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+bigger_model.compile(optimizer='adam',
+                       loss='binary_crossentropy',
+                       metrics=['accuracy', 'binary_crossentropy'])
+
+bigger_history = bigger_model.fit(train_data,
+                                    train_labels,
+                                    epochs=20,
+                                    batch_size=512,
+                                    validation_data=(test_data, test_labels),
+                                    verbose=2)
+
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16, 10))
+
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_' + key],
+                       '--', label=name.title() + ' Val')
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
+                 label=name.title() + ' Train')
+
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_', ' ').title())
+    plt.legend()
+
+    plt.xlim([0, max(history.epoch)])
+
+
+plot_history([('baseline', baseline_history),
+              ('smaller', smaller_history),
+              ('bigger', bigger_history)])
+
+plt.show()
