8cf3fd2c46008879faddb6f5b6428f9c97bdc33c
yunseokddi <oys5339@naver.com>
2021-02-08 09:10:53 +0900

    save and load model tutorial

---
+++ beginner/save_and_load.py
@@ -0,0 +1,26 @@
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+
+def create_model():
+    model = tf.keras.models.Sequential([
+        keras.layers.Dense(512, activation='relu', input_shape=(784,)),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    return model
